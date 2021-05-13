858d38ed2d7566840b2e4a73824321389a804f8f
Andre Natanael da Silva Freitas <andretads24@gmail.com>
2021-05-12 22:36:03 -0300

    Exceção de tabuleiro e programação defensiva

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -6,7 +6,10 @@ public class Board {
 	private Piece[][] pieces;
 	
 	public Board(int rows, int columns) {
-		super();
+		if(rows <1 || columns < 1) {
+			throw new BoardException("Erro ao criar tabuleiro: Deve haver no minimo 1 linha e 1 coluna");
+			
+		}
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
@@ -18,17 +21,13 @@ public class Board {
 		return columns;
 	}
 
-	public void setColumns(int columns) {
-		this.columns = columns;
-	}
+	
 
 	public int getRows() {
 		return rows;
 	}
 
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
+	
 
 	public Piece[][] getPieces() {
 		return pieces;
@@ -38,16 +37,39 @@ public class Board {
 		this.pieces = pieces;
 	}
 	public Piece piece (int row, int column){
-		
+		if (!positionExists(row,column)) {
+			throw new BoardException("Position not on the board");
+		}
 		return pieces[row][column];
 	}
 	public Piece piece (Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Posição inexistente");
+		}
 		return pieces[position.getRow()][position.getColumn()];
 		
 	}
 	public void placePiece(Piece piece, Position position) {
+		if (thereIsAPiece(position)) {
+			throw new BoardException("Ja possui uma peça nessa posição" + position);
+			
+		}
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
 	}
+	private boolean positionExists(int row, int column) {
+		return row >= 0 && row < rows && column >=0 && column < columns;
+	}
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColumn());
+		
+	}
+	public boolean thereIsAPiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Posição inexistente");
+		}
+		return piece(position) != null;
+	}
+	
 
 }
